b764b771bd3360d55c949913173a8bebf3cf8149
fina200 <finnafin9@gmail.com>
2024-07-03 23:46:24 +0800

    perbaikan section1

--- resources/views/arsip/departemen.blade.php
+++ resources/views/arsip/departemen.blade.php
@@ -1,6 +1,24 @@
 @extends('template.main')
 
 @section('content')
+
+<div class="card mt-2 ">
+    <div class="card-header bg-secondary text-white font-weight-bold ">
+        Form Data Departemen
+    </div>
+        <div class="card-body ">
+        <form method = "post" action = "">
+            <div class="form-group ">
+                <label for="nama_departemen">Nama Departemen</label>
+                <input type="text" class="form-control" id="nama_departemen" name="nama_departemen"value="<?=@$vnama_departemen?>"required class = "form-control">
+            </div>
+            <button type="submit" name="bsimpan" class="btn btn-primary">Simpan</button>
+            <button type="reset" name="bbatal" class="btn btn-danger">Batal</button>
+            </form>
+        </div>
+    </div>
+ 
+
 <div class="card mt-2 ">
     <div class="card-header bg-secondary text-white font-weight-bold">
         Data Departemen
