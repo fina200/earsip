d40ba2815496777899bf052e0b11f79030779042
fina200 <finnafin9@gmail.com>
2024-07-03 17:57:45 +0800

    buat controller tamu pengawas

---
+++ app/Http/Controllers/TamuPengawasController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TamuPengawas;
+use Illuminate\Http\Request;
+
+class TamuPengawasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $data=TamuPengawas::all();
+        return view ('tamu.tamudinas', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+
+        $validateData = $request->validate(
+            [
+                'nama' => 'required|max:255',
+                'instansi' => 'required|max:255',
+                'tujuan' => 'required|max:255',
+                'saran' => 'required|max:255',
+            ],
+            [
+                'nama.required' => 'Nama harus diisi',
+                'nama.max' => 'Nama maksimal 255 karakter',
+                'instansi.required' => 'instansi harus diisi',
+                'instansi.max' => 'instansi maksimal 255 karakter',
+                'tujuan.required' => 'tujuan harus diisi',
+                'tujuan.max' => 'tujuan maksimal 255 karakter',
+                'saran.required' => 'saran harus diisi',
+                'saran.max' => 'saran maksimal 255 karakter',
+            ]
+        );
+        TamuPengawas::create($validateData);
+        return redirect('/tamudinas');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
+
