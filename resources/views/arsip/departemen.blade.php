3ed0b8bf4240048db834ea6ff2a1e1128542b3aa
fina200 <finnafin9@gmail.com>
2024-07-04 00:15:08 +0800

    perbaikan section1

--- resources/views/arsip/departemen.blade.php
+++ resources/views/arsip/departemen.blade.php
@@ -1,47 +1,48 @@
 @extends('template.main')
 
 @section('content')
-
-<div class="card mt-2 ">
-    <div class="card-header bg-secondary text-white font-weight-bold ">
-        Form Data Departemen
-    </div>
+    <div class="card mt-2 ">
+        <div class="card-header bg-secondary text-white font-weight-bold">
+            Form departemen
+        </div>
         <div class="card-body ">
-        <form method = "post" action = "{{ url('departemen') }}">
-            @csrf
-            <div class="form-group ">
-                <label for="nama_departemen">Nama Departemen</label>
-                <input type="text" class="form-control" id="nama_departemen" name="nama_departemen"required class = "form-control">
-            </div>
-            <button type="submit" name="bsimpan" class="btn btn-primary">Simpan</button>
-            <button type="reset" name="bbatal" class="btn btn-danger">Batal</button>
+            <form method = "post" action = "{{ url('departemen') }}">
+                @csrf
+                <div class="form-group ">
+                    <label for="nama">Nama</label>
+                    <input type="text" class="form-control" id="nama" name="nama" required class = "form-control">
+                </div>
+                <button type="submit" name="bsimpan" class="btn btn-primary bi bi-save"> Simpan</button>
+                <button type="reset" name="bbatal" class="btn btn-danger bi bi-x-square"> Batal</button>
             </form>
         </div>
     </div>
-<div class="card mt-2 ">
-    <div class="card-header bg-secondary text-white font-weight-bold">
-        Data Departemen
-    </div>
+
+
+    <div class="card mt-2">
+        <div class="card-header bg-secondary text-white font-weight-bold">
+            Data Tamu Dinas
+        </div>
         <div class="card-body">
-            <table class="table table-bordered table-hovered table-striped table">
-                <tr class = text-center>
+            <table class="table table-bordered table-hovered table-striped">
+                <tr class=text-center>
                     <th>No</th>
-                    <th>Nama Departemen</th>
+                    <th>Nama</th>
                     <th>Aksi</th>
                 </tr>
                 @foreach ($data as $d)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $d->nama }}</td>
-                    <td>
-                        <a href="?halaman=tamu_dinas&hal=edit&id=" class="btn btn-outline-success bi bi-pencil"></a>
-                        <a href="?halaman=tamu_dinas&hal=hapus&id=" class="btn btn-outline-danger bi bi-trash"
-                            onclick="return confirm ('Apakah anda yakin ingin menghapus data in?')"> </a>
-                    </td>
-                <tr>
-            @endforeach
-        </table>
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $d->nama }}</td>
+                        <td>
+                            <a href="?halaman=tamu_dinas&hal=edit&id=" class="btn btn-outline-success bi bi-pencil"></a>
+                            <a href="?halaman=tamu_dinas&hal=hapus&id=" class="btn btn-outline-danger bi bi-trash"
+                                onclick="return confirm ('Apakah anda yakin ingin menghapus data in?')"> </a>
+                        </td>
+                    <tr>
+                @endforeach
+            </table>
+        </div>
     </div>
-</div>
-</div>
+    </div>
 @endsection
