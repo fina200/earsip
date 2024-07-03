febb8166df3d728d14ab0db150407bd82174dd1b
fina200 <finnafin9@gmail.com>
2024-07-03 18:08:25 +0800

    ubah import controller tamu pengawas

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
-use App\Http\Controllers\TamuDinasController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TamuUmumController;
+use App\Http\Controllers\TamuDinasController;
+use App\Http\Controllers\TamuPengawasController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('/tamudinas', TamuDinasController::class); 
+Route::resource('/tamupengawas', TamuPengawasController::class);
+Route::resource('/tamuumum', TamuUmumController::class); 
+
